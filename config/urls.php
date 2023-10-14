c61b0eebf88d0489ef48d45c476c28be782eaa04
Dip Ghosh <dip.ghosh@shohoz.com>
2023-10-14 13:49:48 +0600

    sub domain configure

---
+++ config/urls.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'ADMIN_DOMAIN' => 'admin.launch.test',
+    'API_DOMAIN'   => 'api.launch.test',
+//    'MAIN_DOMAIN'  => 'launch.test',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//Route::group( [ 'domain' => Config::get('urls.ADMIN_DOMAIN')], function () {
+//    require base_path('routes/admin.php');
+//});
+
+Route::group( [ 'domain' => Config::get('urls.API_DOMAIN')], function () {
+    require base_path('routes/api.php');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
